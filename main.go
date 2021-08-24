6fc59d534fc009b9c524305375095dde098fdff3
molson82 <wvandebelt82@yahoo.com>
2021-08-23 20:43:35 -0400

    :tada: refactor rpc server setup & test

--- main.go
+++ main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net"
 	"net/http"
-	"strings"
 
-	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
-	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
-	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
-	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
-	grpc_reqAuth "github.com/hypebid/go-kit/grpc/middleware/auth"
-	grpc_reqId "github.com/hypebid/go-kit/grpc/middleware/transactionId"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/sirupsen/logrus"
 
 	"github.com/hypebid/go-micro-template/internal/config"
 	"github.com/hypebid/go-micro-template/internal/rpc"
-	"github.com/hypebid/go-micro-template/internal/rpc/pb"
-	"google.golang.org/grpc"
 )
 
 func metrics() {
@@ -34,40 +22,19 @@ func main() {
 		return
 	}
 
-	logOpts := []grpc_logrus.Option{}
-	recovOpts := []grpc_recovery.Option{}
-	reqAuthOpts := grpc_reqAuth.Options{
-		HashSecret:      c.Constants.HashSecret,
-		MetadataKeyList: strings.Split(c.Constants.MetadataKeyList, ","),
-		MetadataHashKey: c.Constants.MetadataHashKey,
-	}
+	c.Log.Info("starting metrics route...")
+	go metrics()
+	c.Log.Info("done")
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", c.Constants.Port))
+	c.Log.Info("setting up grpc server...")
+	lis, grpcServer, err := rpc.RpcSetup(c)
 	if err != nil {
-		c.Log.Fatalf("Failed to listen: %v\n", err)
+		c.Log.Error("error setting up rpc server: ", err)
 		return
 	}
+	c.Log.Info("done")
 
-	grpcServer := grpc.NewServer(
-		grpc_middleware.WithStreamServerChain(
-			grpc_reqId.StreamServerInterceptor(c.Log),
-			grpc_reqAuth.StreamServerInterceptor(c.Log),
-			grpc_ctxtags.StreamServerInterceptor(),
-			grpc_logrus.StreamServerInterceptor(logrus.NewEntry(c.Log), logOpts...),
-			grpc_recovery.StreamServerInterceptor(recovOpts...)),
-		grpc_middleware.WithUnaryServerChain(
-			grpc_reqId.UnaryServerInterceptor(c.Log),
-			grpc_reqAuth.UnaryServerInterceptor(c.Log, reqAuthOpts),
-			grpc_ctxtags.UnaryServerInterceptor(),
-			grpc_logrus.UnaryServerInterceptor(logrus.NewEntry(c.Log), logOpts...),
-			grpc_recovery.UnaryServerInterceptor(recovOpts...)),
-	)
-
-	go metrics()
-
-	pb.RegisterServiceNameServer(grpcServer, &rpc.Server{Config: c})
-
-	c.Log.Printf("Server listening on %v", c.Constants.Port)
+	c.Log.Info("Server listening on ", c.Constants.Port)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		c.Log.Fatalf("failed to serve: %v\n", err)
--- tests/healthcheck_test.go
+++ tests/healthcheck_test.go
@@ -4,19 +4,11 @@ import (
 	"context"
 	"log"
 	"net"
-	"strings"
 	"testing"
 
-	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
-	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
-	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
-	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
-	grpc_reqAuth "github.com/hypebid/go-kit/grpc/middleware/auth"
-	grpc_reqId "github.com/hypebid/go-kit/grpc/middleware/transactionId"
 	"github.com/hypebid/go-micro-template/internal/config"
 	"github.com/hypebid/go-micro-template/internal/rpc"
 	"github.com/hypebid/go-micro-template/internal/rpc/pb"
-	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/test/bufconn"
 )
@@ -30,31 +22,15 @@ func init() {
 	if err != nil {
 		log.Fatalf("failed to create config")
 	}
-	logOpts := []grpc_logrus.Option{}
-	recovOpts := []grpc_recovery.Option{}
-	reqAuthOpts := grpc_reqAuth.Options{
-		HashSecret:      c.Constants.HashSecret,
-		MetadataKeyList: strings.Split(c.Constants.MetadataKeyList, ","),
-		MetadataHashKey: c.Constants.MetadataHashKey,
-	}
+
 	lis = bufconn.Listen(bufSize)
-	s := grpc.NewServer(
-		grpc_middleware.WithStreamServerChain(
-			grpc_reqId.StreamServerInterceptor(c.Log),
-			grpc_reqAuth.StreamServerInterceptor(c.Log),
-			grpc_ctxtags.StreamServerInterceptor(),
-			grpc_logrus.StreamServerInterceptor(logrus.NewEntry(c.Log), logOpts...),
-			grpc_recovery.StreamServerInterceptor(recovOpts...)),
-		grpc_middleware.WithUnaryServerChain(
-			grpc_reqId.UnaryServerInterceptor(c.Log),
-			grpc_reqAuth.UnaryServerInterceptor(c.Log, reqAuthOpts),
-			grpc_ctxtags.UnaryServerInterceptor(),
-			grpc_logrus.UnaryServerInterceptor(logrus.NewEntry(c.Log), logOpts...),
-			grpc_recovery.UnaryServerInterceptor(recovOpts...)),
-	)
-	pb.RegisterServiceNameServer(s, &rpc.Server{Config: c})
+	_, grpcServer, err := rpc.RpcSetup(c)
+	if err != nil {
+		log.Fatalf("failed to setup rpc server")
+	}
+
 	go func() {
-		if err := s.Serve(lis); err != nil {
+		if err := grpcServer.Serve(lis); err != nil {
 			log.Fatalf("Server exited with error: %v", err)
 		}
 	}()
